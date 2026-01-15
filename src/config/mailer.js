7d8a78b93f354cfcd645332d7721cb74000840f8
yagyasoni <yagyasony@gmail.com>
2026-01-15 10:52:42 +0530

    extra

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -4,8 +4,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async ({ subject, html }) => {
   return await resend.emails.send({
-    from: process.env.FROM_EMAIL,
-    to: process.env.RECEIVER_EMAIL,
+    from: `RX Flow <${process.env.FROM_EMAIL}>`,
+    to: [process.env.RECEIVER_EMAIL],
     subject,
     html,
   });
